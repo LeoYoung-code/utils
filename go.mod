0ffccb4f5af3b2cabcbb328918b94d880674c41e
leo <450019458@qq.com>
2023-03-27 11:15:38 +0800

    feat(all):new util

--- go.mod
+++ go.mod
@@ -15,13 +15,18 @@ require (
 	github.com/shopspring/decimal v1.3.1
 	github.com/spf13/cast v1.5.0
 	github.com/stretchr/testify v1.8.2
+	xorm.io/xorm v1.3.2
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/goccy/go-json v0.8.1 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/syndtr/goleveldb v1.0.0 // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	xorm.io/builder v0.3.11-0.20220531020008-1bd24a7dc978 // indirect
 )
---
+++ transaction.go
@@ -0,0 +1,84 @@
+package utils
+
+import (
+	"context"
+	"runtime"
+
+	"github.com/pkg/errors"
+	"xorm.io/xorm"
+)
+
+// 出于对事务处理，将多个表的事务同时处理
+
+// TransactionUtil 主要是为了service可以方便的调用事务而做出来的一个结构体
+type TransactionUtil interface {
+	// Transaction
+	//  开启一个非自动提交的事务, 可以嵌套调用，每次都会从ctx中找是否有合适的事务，如果没有
+	//  合适的则会新建一个事务，并且注入到ctx，开启一个事务，任意一个fc抛出错误都会进行回滚
+	//  ctx 上下文，会尝试从ctx中获取事务
+	Transaction(ctx context.Context, fc func(ctx context.Context) error) error
+
+	// WithContextForTransaction
+	//  repo 自己内部使用的方法，如果你希望你的repo方法支持上面那种事务,那么需要用这个方法来获取
+	// 	session。  先尝试从这个ctx获取session，如果获取到了就返回这个session，如果没有获取
+	//	到session，会自己创建一个新自动提交的session
+	WithContextForTransaction(ctx context.Context) *xorm.Session
+}
+
+type (
+	MysqlClient xorm.EngineInterface
+)
+
+type transactionImpl struct {
+	mysqlClient MysqlClient // 这里的mysqlClient是一个接口，里面有一个NewSession()方法，返回一个*xorm.Session
+}
+
+func (t *transactionImpl) Transaction(ctx context.Context, fc func(ctx context.Context) error) (err error) {
+
+	if _, ok := t.getTx(ctx); ok {
+		// 已经有事务了，继续执行就好了
+		return fc(ctx)
+	}
+	tx := t.mysqlClient.NewSession()
+	if err = tx.Begin(); err != nil {
+		err = errors.WithMessage(err, "[TransactionImpl.Transaction] begin transaction failed")
+		return
+	}
+	defer func() {
+		if e := recover(); e != nil {
+			buf := make([]byte, 4096)
+			buf = buf[:runtime.Stack(buf, false)]
+			err = errors.Errorf("db transaction panic: %v, stack: \n%s", e, buf)
+		}
+		if err != nil {
+			_ = tx.Rollback()
+		}
+	}()
+
+	ctx = context.WithValue(ctx, t.mysqlClient, tx)
+	err = fc(ctx)
+	if err == nil {
+		err = tx.Commit()
+	}
+	return
+}
+
+func (t *transactionImpl) getTx(ctx context.Context) (*xorm.Session, bool) {
+	txAny := ctx.Value(t.mysqlClient)
+	tx, ok := txAny.(*xorm.Session)
+	if !ok {
+		return nil, false
+	}
+	return tx, true
+}
+
+func (t *transactionImpl) WithContextForTransaction(ctx context.Context) *xorm.Session {
+	tx, ok := t.getTx(ctx)
+	if ok {
+		// 已经有tx了，用这个tx即可
+		return tx
+	}
+	// 没有tx, 返回一个自动提交的transaction
+	return t.mysqlClient.Context(ctx)
+
+}
