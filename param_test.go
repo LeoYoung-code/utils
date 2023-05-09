59e1601cae6e7e138e9dc0d489f17f25a58bfce8
leo <450019458@qq.com>
2023-05-09 16:03:40 +0800

    feat:add params

---
+++ param_test.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetSysVersion(t *testing.T) {
+	type args struct {
+		versionStr string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		{
+			name: "test1",
+			args: args{
+				versionStr: "10.2",
+			},
+			want: 10,
+		},
+		{
+			name: "test2",
+			args: args{
+				versionStr: "10.10.5",
+			},
+			want: 10,
+		},
+		{
+			name: "test3",
+			args: args{
+				versionStr: "",
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fmt.Println(GetSysVersion(tt.args.versionStr))
+			assert.Equalf(t, tt.want, GetSysVersion(tt.args.versionStr), "GetSysVersion(%v)", tt.args.versionStr)
+		})
+	}
+}
