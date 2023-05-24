664bdd345bf6a6f337227b0d564e818d0234987f
leo <450019458@qq.com>
2023-05-24 20:00:50 +0800

    feat:add

--- cache.go
+++ cache.go
@@ -10,10 +10,12 @@ import (
 
 var cacheDriver = cache.New(1*time.Minute, 10*time.Minute)
 
+// SetGoCache set cache
 func SetGoCache[T any](key string, val T, exp time.Duration) {
 	cacheDriver.Set(key, val, exp)
 }
 
+// GetGoCache get cache
 func GetGoCache[T any](key string, defaultVal T) (T, bool) {
 	val, ok := cacheDriver.Get(key)
 	if !ok {
@@ -22,10 +24,12 @@ func GetGoCache[T any](key string, defaultVal T) (T, bool) {
 	return val.(T), ok
 }
 
+// DelGoCache delete cache
 func DelGoCache(key string) {
 	cacheDriver.Delete(key)
 }
 
+// DeepCopy deep copy
 func DeepCopy(to, form any) {
 	err := copier.CopyWithOption(to, form, copier.Option{
 		IgnoreEmpty: true,
