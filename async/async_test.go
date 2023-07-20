76788f120167952dfc0beb34a882d407d0a53a4d
leo <450019458@qq.com>
2023-07-20 16:34:38 +0800

    feat:调整

--- async/async_test.go
+++ async/async_test.go
@@ -6,8 +6,8 @@ import (
 	"testing"
 	"time"
 
-	"codeup.aliyun.com/qimao/leo/lib/log"
 	"github.com/getsentry/sentry-go"
+	"github.com/go-kratos/kratos/v2/log"
 	"github.com/pkg/errors"
 	"golang.org/x/sync/errgroup"
 )
