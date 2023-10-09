70f48a1498b31720071f75fb6a15d6d254845890
leo <450019458@qq.com>
2023-10-09 19:40:21 +0800

    test:unit

--- string/strings_test.go
+++ string/strings_test.go
@@ -13,7 +13,27 @@ func TestGoSanitized(t *testing.T) {
 		args args
 		want string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "1",
+			args: args{
+				s: "hello world",
+			},
+			want: "hello_world",
+		},
+		{
+			name: "2",
+			args: args{
+				s: "hello world a",
+			},
+			want: "hello_world_a",
+		},
+		{
+			name: "3",
+			args: args{
+				s: "hello",
+			},
+			want: "hello",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
