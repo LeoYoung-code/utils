24ceab6f1f09326c7e24f5c979c4c37a540102a5
leo <450019458@qq.com>
2023-10-16 17:39:05 +0800

    feat:ones

---
+++ channel/sync_ones.go
@@ -0,0 +1,47 @@
+package channel
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+type Foo struct {
+}
+
+var once sync.Once
+var instance *Foo
+
+func getInstance(id int) *Foo {
+	defer func() {
+		if e := recover(); e != nil {
+			log.Printf("goroutine-%d: caught a panic: %s", id, e)
+		}
+	}()
+	log.Printf("goroutine-%d: enter GetInstance\n", id)
+	once.Do(func() {
+		instance = &Foo{}
+		time.Sleep(3 * time.Second)
+		log.Printf("goroutine-%d: the addr of instance is %p\n", id, instance)
+		panic("panic in once.Do function")
+	})
+	return instance
+}
+
+func testOnes() {
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			inst := getInstance(i)
+			log.Printf("goroutine-%d: the addr of instance returned is %p\n", i, inst)
+			wg.Done()
+		}(i + 1)
+	}
+	time.Sleep(5 * time.Second)
+	inst := getInstance(0)
+	log.Printf("goroutine-0: the addr of instance returned is %p\n", inst)
+
+	wg.Wait()
+	log.Printf("all goroutines exit\n")
+}
