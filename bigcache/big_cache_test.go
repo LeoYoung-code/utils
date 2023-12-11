e0c8230c14c63f326d19f94e386e320cb71b5bf9
leo <450019458@qq.com>
2023-12-11 19:13:21 +0800

    test: cache

--- bigcache/big_cache_test.go
+++ bigcache/big_cache_test.go
@@ -1,10 +1,13 @@
 package bigcache
 
 import (
+	"context"
 	"fmt"
 	"runtime"
 	"testing"
 	"time"
+
+	"github.com/allegro/bigcache/v3"
 )
 
 func TestSetGoCache(t *testing.T) {
@@ -29,4 +32,39 @@ func printMemUsage() {
 
 func bToMb(b uint64) uint64 {
 	return b / 1024 / 1024
+}
+
+// mockBigcache is a mock implementation of Bigcache
+type mockBigcache struct {
+}
+
+func (*mockBigcache) New(ctx context.Context, config bigcache.Config) (*bigcache.BigCache, error) {
+	return &bigcache.BigCache{}, nil
+}
+
+func TestNewCache(t *testing.T) {
+	tests := []struct {
+		name string
+		mock func() *mockBigcache
+		want *bigcache.BigCache
+	}{
+		{
+			name: "Successful initialization",
+			mock: func() *mockBigcache {
+				return &mockBigcache{}
+			},
+			want: &bigcache.BigCache{},
+		},
+		// Here you can add more tests cases, for example, when the initialization fails
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctx := context.Background()
+			got := NewCache(ctx)
+			if got == nil {
+				t.Error("Cache should not be nil")
+			}
+		})
+	}
 }
